c9c3e324d99bcf25f41d4aac0a267f91227e3872
yeongjinc <yeongjinc@gmail.com>
2017-05-13 18:26:29 +0900

    thread with timer, enable ctrl c, append json file

--- crawler.py
+++ crawler.py
@@ -1,19 +1,25 @@
 import praw
 import json
+import threading
+import time
+import os
 
-CRAWLING_PERIOD_SEC = 10
+CRAWLING_PERIOD_SEC = 3600
 CLIENT_SECRET = 'client.secret'
 
-SUBREDDIT_LIST = ['news']
+SUBREDDIT_LIST = ['news', 'uncensorednews', 'worldnews', 'Upliftingnews',
+                    'truenews', 'fakenews', 'politicalnews']
 POSTS_FILE = 'posts.txt'
 COMMENTS_FILE = 'comments.txt'
+
+INF_TIME = 3600*24*30 # a month
 
 def getReddit(c_id, c_secret, u_agent):
     reddit = praw.Reddit(client_id=c_id, client_secret=c_secret, user_agent=u_agent);
     return reddit;
 
 def serialize(subs_list):
-    return json.dumps(subs_list)
+    return json.dumps(subs_list, indent=2)
 
 def crawler(reddit, start_date, end_date):
     subs_list = []
@@ -43,14 +49,47 @@ def crawler(reddit, start_date, end_date):
                 comments_list.append(comment_dic)
     return (serialize(subs_list), serialize(comments_list))
 
-#def timer(reddit, period):
+def append_to_json(json_file, data):
+    s = str(data)
+    json_file.seek(0, os.SEEK_END)
+    pos = json_file.tell() - 2
+    if pos > 0:
+        json_file.seek(pos, os.SEEK_SET)
+        json_file.truncate()
+        s = ',' + s[1:]
+    json_file.write(s)
+
+def timer(reddit, posts_file, comments_file):
+    while True:
+        end_date = int(time.time())
+        start_date = end_date - CRAWLING_PERIOD_SEC
+        res = crawler(reddit, start_date, end_date)
+        append_to_json(posts_file, res[0])
+        append_to_json(comments_file, res[1])
+        time.sleep(CRAWLING_PERIOD_SEC)
+        # time.sleep(10) #test
 
 
 if __name__ == "__main__":
     cs_file = open(CLIENT_SECRET, 'r')
     lines = cs_file.read().splitlines()
+    cs_file.close()
+
     reddit = getReddit(lines[2], lines[3], lines[4])
 
+    p_f = open(POSTS_FILE, 'a+')
+    c_f = open(COMMENTS_FILE, 'a+')
+
+    cthread = threading.Thread(target=timer,args=[reddit, p_f, c_f])
+    cthread.daemon = True
+    cthread.start()
+
+    try:
+        time.sleep(INF_TIME)
+    except KeyboardInterrupt:
+        print 'over'
+    p_f.close()
+    c_f.close()
 
 
 
